3f43745739d45ca0046a7370c9a768d005d53368
Santiago <velozraptorblu@gmail.com>
2025-02-18 09:56:55 -0500

    Deuda de Danny actualizada

--- deudas/script.js
+++ deudas/script.js
@@ -17,13 +17,21 @@ const userDetails = {
     Danny: [
         {Deuda: "Deuda 1: 2.000 Pesos", info: "Rodar la cama (20/01/2025)//(27/01/2025)--(20/02/2025)"},
 
-        {Deuda: "Penalización: 200%", info: "Pasaron 7 dias, la proxima penalizacion (3/02/2025)--(20/02/2025)"},
+        {Deuda: "Penalización (deuda1): 200%", info: "Pasaron 7 dias, la proxima penalizacion (3/02/2025)--(20/02/2025)"},
 
-        {Deuda: "Penalización: 150%", info: "Pasaron 2 semanas (14 dias), la proxima penalizacion (10/02/2025)--(20/02/2025)"},
+        {Deuda: "Penalización (deuda1): 150%", info: "Pasaron 2 semanas (14 dias), la proxima penalizacion (10/02/2025)--(20/02/2025)"},
+
+        {Deuda: "Penalización (deuda1): 150%", info: "Pasaron 3 semanas (21 dias), la ultima penalización (20/02/2025)"},
 
         {Deuda: "Deuda 2: 10.000 Pesos", info: "Ir al sao a sacar dinero (05/02/2025)//(12/02/2025)--(05/03/2025)"},
 
-        {Deuda: "Total: 16.000 Pesos", info: "Penalización añadida"}
+        {Deuda: "Penalización (deuda2): 200%", info: "Pasaron 7 dias, la proxima penalización (19/02/2025)--(05/03/2025)"},
+
+        {Deuda: "Penalización (deuda2): 150%", info: "Pasaron 2 semanas (14 dias), la proxima penalización (26/02/2025)--(05/03/2025)"},
+
+        {Deuda: "Adelantó 40 mil pesos", info: "Fue para estudio pero igual se lo agrego acá-."},
+
+        {Deuda: "Total: 17 mil Pesos", info: "Penalización añadida + 40k"}
     ],
     Sophia: [
         {Deuda: "No ha solicitado", info:"N/A"}
